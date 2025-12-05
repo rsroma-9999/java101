633c0428c9e20b0fa5049a4aa84e1755158c02e0
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-05 18:32:48 +0800

    M2 Activity4

---
+++ Executable.java
@@ -0,0 +1,7 @@
+package Act4;
+
+public interface Executable {
+	   // seatwork: add your methods here
+		void run();
+		void stop();
+	}
---
+++ MSWord.java
@@ -0,0 +1,14 @@
+package Act4;
+
+public class MSWord implements Executable {
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+	}
+}
