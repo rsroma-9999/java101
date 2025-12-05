1d540ff5106890b157fd0000cd67a7dfdfcdd9ae
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-05 18:41:56 +0800

    M2 Activity2

---
+++ Book.java
@@ -0,0 +1,12 @@
+package Seatwork1;
+	
+		public class Book {
+			String title = "The Lord of the Rings"; // attribute
+			int pages = 999; // attribute
+			
+			void details() { // method
+				System.out.println("This book is about " + title);
+				System.out.println("This book have " + pages + " pages.");
+				
+		}
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+package Seatwork1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Book read1 = new Book();
+     read1.details(); 
+	}
+
+}
