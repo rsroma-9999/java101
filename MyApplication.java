0e64c0bce14d54aca9b2a845221fb6fac24d9bbd
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-10 14:54:29 +0800

    M2 Activity6

---
+++ MyApplication.java
@@ -0,0 +1,10 @@
+package Act6;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Refuelable.java
@@ -0,0 +1,5 @@
+package Act6;
+
+public class Refuelable {
+
+}
