2e975c462bf3a2633f7e2984495fa92f70d6d8fe
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-11 14:05:00 +0800

    M2 Activity2

---
+++ Seatwork2.java
@@ -0,0 +1,12 @@
+package Seatwork2;
+
+public class Seatwork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhiteBoard wb = new WhiteBoard();
+		int total = wb.addNumbers(5,3);
+		 System.out.println("The total is " + String.valueOf(total) );
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,8 @@
+package Seatwork2;
+
+public class WhiteBoard {
+    int addNumbers(int addend1, int addend2) {
+	int sum = addend1 + addend2;
+	return sum;
+    }
+}
