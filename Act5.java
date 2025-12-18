17e7556bf4e049d0cc217ed044d24a90d5e69f7c
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-18 13:44:08 +0800

    M1 Activity5

---
+++ Act5.java
@@ -0,0 +1,17 @@
+package Day2;
+// import java.util.Scanner;
+public class Act5 {
+
+	public static void main(String[] args) {
+		    			int sum = 0; // init sum
+		    			
+		  // loop until 50  			
+		        for (int i1 = 1; i1 <= 50; i1++) {
+		 		        sum += i1; 
+		        } // end of for loop
+		       
+		        System.out.println("Sum: " + sum);  // print sum
+		 
+	} // end of main
+
+} // end of main class
