89485029cca655ae2c2abdabd253c3f257b7c673
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2025-12-18 13:32:14 +0800

    M1 Activity1

---
+++ Test01.java
@@ -0,0 +1,15 @@
+package Test01;
+import java.util.Scanner;
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("What is your Name? "); 
+		 String name = scanner.nextLine();
+		 System.out.println("Hello " + name + "!"); 
+		 scanner.close();
+		 }
+	}
+
+
