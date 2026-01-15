57c197ef888f4020504fbf31133e884696a4fa4c
Richard Mark S. Roma <rmsroma@headoffice.corp.bpi.com.ph>
2026-01-15 15:08:40 +0800

    M4 Activity7

---
+++ BankTestOperation.java
@@ -0,0 +1,12 @@
+package act7;
+
+@FunctionalInterface
+	public interface BankTestOperation {
+    	void execute() throws InvalidAmountException, InsufficientFundsException;
+	
+	}
+
+
+	
+
+
---
+++ InvalidAmountException.java
@@ -0,0 +1,10 @@
+package act7;
+
+@SuppressWarnings("serial")
+public class InvalidAmountException extends Exception {
+
+	public InvalidAmountException(String message) {
+        super(message);
+    }
+	
+}
